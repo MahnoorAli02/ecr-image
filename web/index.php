40a977001f106947ae1326ca1f6145b39106a1b2
MahnoorAli02 <96840071+MahnoorAli02@users.noreply.github.com>
2022-08-16 14:09:39 +0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Welcome to sherdilIT Academy";
+echo "Welcome to sherdilIT Academy !!!";
 
 echo print_r($_ENV);
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
